af5995c7111e19b17abd93cafee180df57d728e4
Manuel Kamper <kamp@noreply.kmpr.at>
2022-12-18 15:23:23 +0000

    Dateien hochladen nach „“

---
+++ Keypad.py
@@ -0,0 +1,27 @@
+from machine import Pin
+import utime as time
+
+debounce_time = 0
+
+class Keypad():
+    def __init__(self):
+        self.taste1 = Pin(19, Pin.IN, Pin.PULL_UP)
+        self.taste2 = Pin(18, Pin.IN, Pin.PULL_UP)
+        self.taste3 = Pin(21, Pin.IN, Pin.PULL_UP)
+        self.taste4 = Pin(20, Pin.IN, Pin.PULL_UP)
+        
+        self.taste1.irq(trigger=Pin.IRQ_FALLING, handler=self.callback)
+        self.taste2.irq(trigger=Pin.IRQ_FALLING, handler=self.callback)
+        self.taste3.irq(trigger=Pin.IRQ_FALLING, handler=self.callback)
+        self.taste4.irq(trigger=Pin.IRQ_FALLING, handler=self.callback)
+        
+        self.interrupt_flag = 0
+        self.tastePressed = self.taste1
+    
+    # Keypress and debouncing
+    def callback(self, pin):
+        global debounce_time
+        if (time.ticks_ms() - debounce_time) > 300:
+            self.interrupt_flag = 1
+            debounce_time = time.ticks_ms()
+            self.tastePressed = pin
--- main.py
+++ main.py
@@ -1,157 +1,36 @@
 import rp2
-import network
-import ubinascii
-import machine
-import urequests as requests
-import time
+import TimeUtils
+import Logger
+import Oled
+import Networking
+import NTP
+import uasyncio as asyncio
+import utime as time
+import Keypad
+
 from secrets import secrets
 from configs import configs
-import sys
-import socket
-import utime as time
-import ustruct as struct
-import framebuf
-import Oled
-import uasyncio as asyncio
-import uos as os
-from machine import Pin
 
 rp2.country(configs['country'])
-wlan = network.WLAN(network.STA_IF)
-mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
 version = "0.2-alpha"
-led = machine.Pin('LED', machine.Pin.OUT)
+
 Oled = Oled.Oled()
+TimeUtils = TimeUtils.TimeUtils()
+Logger = Logger.Logger(configs['log_housekeeping_days'])
+Networking = Networking.Networking(Logger, secrets['ssid'], secrets['pw'])
+NTP = NTP.NTP(Logger)
+Keypad = Keypad.Keypad()
+
 boottime = time.time()
-interrupt_flag = 0
-debounce_time = 0
-taste1 = Pin(19, Pin.IN, Pin.PULL_UP)
-taste2 = Pin(18, Pin.IN, Pin.PULL_UP)
-taste3 = Pin(21, Pin.IN, Pin.PULL_UP)
-taste4 = Pin(20, Pin.IN, Pin.PULL_UP)
-tasteA = taste1
+lastActionTicks = 0
 subscreen = 0
 sc = 0
 partyMode = False
-lastActionTicks = 0
 displayOff = False
-
-def callback(pin):
-    global interrupt_flag, debounce_time, tasteA
-    if (time.ticks_ms() - debounce_time) > 300:
-        interrupt_flag = 1
-        debounce_time = time.ticks_ms()
-        tasteA = pin
-
-taste1.irq(trigger=Pin.IRQ_FALLING, handler=callback)
-taste2.irq(trigger=Pin.IRQ_FALLING, handler=callback)
-taste3.irq(trigger=Pin.IRQ_FALLING, handler=callback)
-taste4.irq(trigger=Pin.IRQ_FALLING, handler=callback)
-
-# WLAN-Verbindung herstellen
-def wlanConnect():
-    wlan.active(True)
-    #wlan.config(hostname=configs['hostname'])
-    if(configs['disable_wifi_powersavingmode']):
-        wlan.config(pm = 0xa11140)
-    Log("mac = " + str(mac))
-    Log("channel = " + str(wlan.config('channel')))
-    Log("essid = " + str(wlan.config('essid')))
-    Log("txpower = " + str(wlan.config('txpower')))
-    wlan.connect(secrets['ssid'], secrets['pw'])
-    timeout = 10
-    while timeout > 0:
-        if wlan.status() < 0 or wlan.status() >= 3:
-            break
-        timeout -= 1
-        Log('Waiting for connection...')
-        time.sleep(1)
-
-# Blinkfunktion der Onboard-LED für Errorcodes   
-def blink_onboard_led(num_blinks):
-    for i in range(num_blinks):
-        led.on()
-        time.sleep(.2)
-        led.off()
-        time.sleep(.2)
-        
-# Wlan Status on LED
-# Handle connection error
-# Error meanings
-# 0  Link Down
-# 1  Link Join
-# 2  Link NoIp
-# 3  Link Up
-# -1 Link Fail
-# -2 Link NoNet
-# -3 Link BadAuth
-def wlanStatus():
-    wlan_status = wlan.status()
-    blink_onboard_led(wlan_status)
-    if wlan_status != 3:
-        #raise RuntimeError('Wi-Fi connection failed')
-        return False
-    else:
-        Log('Connected')
-        status = wlan.ifconfig()
-        Log('ip = ' + status[0])
-        return True
-
-# Zeit von NTP holen und setzen
-def setRTCTimeNTP():
-    Log("getting date and time from NTP " + configs['ntp_host'])
-    NTP_DELTA = 2208988800
-    NTP_QUERY = bytearray(48)
-    NTP_QUERY[0] = 0x1B
-    addr = socket.getaddrinfo(configs['ntp_host'], 123)[0][-1]
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.settimeout(5)
-        res = s.sendto(NTP_QUERY, addr)
-        msg = s.recv(48)
-    except Exception as err:
-        Log("Error: " + str(err))
-        return False
-    finally:
-        s.close()
-    ntp_time = struct.unpack("!I", msg[40:44])[0] + (3600 * configs['gmt_offset']) #gmt offset from config
-    tm = time.gmtime(ntp_time - NTP_DELTA)
-    if (configs['auto_summertime']):
-        Log("performing auto_summertime adjustment")
-        #Time of March change for the current year
-        t1 = time.mktime((tm[0],3,(31-(int(5*tm[0]/4+4))%7),1,0,0,0,0))
-        #Time of October change for the current year
-        t2 = time.mktime((tm[0],10,(31-(int(5*tm[0]/4+1))%7),1,0,0,0,0))
-        t = time.mktime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
-        if t >= t1 and t < t2:
-            tm = time.gmtime(tm + 3600)
-    machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
-    return True
-
-# Zeigt Text am Display an
-def ShowText(line1, line2, line3):
-    Oled.fill(0x0000)
-    Oled.text(line1,1,2,Oled.white)
-    Oled.text(line2,1,12,Oled.white)
-    Oled.text(line3,1,22,Oled.white)  
-    Oled.show()
-
-# Loggt messages in täglichen Logfiles im Ordner logs
-def Log(message):
-    print(message)
-    dt = machine.RTC().datetime()
-    file = open("/logs/"+str(dt[0])+"-"+str(dt[1])+"-"+str(dt[2])+".txt", "a")
-    file.write(DateTimeNow() + ";" + message + "\n")
-    file.close()
-
-# Funktion, die das aktuelle Datum und Uhrzeit zurück gibt
-def DateTimeNow():
-    tm = time.gmtime()
-    return "%02d.%02d.%04d,%02d:%02d" % (tm[2], tm[1], tm[0], tm[3], tm[4])
 
 # Führt einen Reboot des Pico aus (z.B. im Fehlerfall)
 def Reboot():
-    Log("Performing Reboot")
+    Logger.LogMessage("Performing Reboot")
     machine.reset()
 
 # Berechnet die Uptime in Sekunden
@@ -164,54 +43,13 @@ def Uptime():
     seconds %= 60
     return "%d:%02d" % (hours, minutes)
 
-# Löscht alle Logs die nicht von heute sind
-def HousekeepingLogs():
-    for file in os.listdir("/logs"):
-        if(file.endswith(".txt")):
-            print(file)
-            #todo delete files older than x days
-
-# API
-html = """<!DOCTYPE html>
-<html>
-    <head> <title>Pico W</title> </head>
-    <body> <h1>Pico W</h1>
-        <p>%s</p>
-    </body>
-</html>
-"""
-async def ServeApi(reader, writer):
-    print("Client connected")
-    request_line = await reader.readline()
-    print("Request:", request_line)
-    # We are not interested in HTTP request headers, skip them
-    while await reader.readline() != b"\r\n":
-        pass
-
-    request = str(request_line)
-    led_on = request.find('/light/on')
-    led_off = request.find('/light/off')
-    print( 'led on = ' + str(led_on))
-    print( 'led off = ' + str(led_off))
-
-    stateis = ""
-    if led_on == 6:
-        print("led on")
-        led.on()
-        stateis = "LED is ON"
-    
-    if led_off == 6:
-        print("led off")
-        led.off()
-        stateis = "LED is OFF"
-        
-    response = html % stateis
-    writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-    writer.write(response)
-
-    await writer.drain()
-    await writer.wait_closed()
-    print("Client disconnected")
+# Zeigt Text am Display an
+def ShowText(line1, line2, line3):
+    Oled.fill(0x0000)
+    Oled.text(line1,1,2,Oled.white)
+    Oled.text(line2,1,12,Oled.white)
+    Oled.text(line3,1,22,Oled.white)  
+    Oled.show()
 
 # Bildschirmausgabe
 def BuildScreen():
@@ -226,7 +64,7 @@ def BuildScreen():
         ShowText("","","")
     else:
         if (subscreen == 0):
-            ShowText("TCS<->FHEM", DateTimeNow(), "Auf LiG PaM Chk")
+            ShowText("TCS<->FHEM", TimeUtils.DateTimeNow(), "Auf LiG PaM Chk")
         elif (subscreen == 1):
             ShowText("Eingangstuer", "getriggert", "            Ext")
         elif (subscreen == 2):
@@ -236,9 +74,9 @@ def BuildScreen():
         elif (subscreen == 4):
             ShowText("Hostname:", configs['hostname'], "Up  Dwn     Ext")
         elif (subscreen == 5):
-            ShowText("MAC Address:", ubinascii.hexlify(network.WLAN().config('mac'),':').decode(), "Up  Dwn     Ext")
+            ShowText("MAC Address:", Networking.GetMACAddress(), "Up  Dwn     Ext")
         elif (subscreen == 6):
-            ShowText("IP Address:", wlan.ifconfig()[0], "Up  Dwn     Ext")
+            ShowText("IP Address:", Networking.GetIPAddress(), "Up  Dwn     Ext")
         elif (subscreen == 7):
             ShowText("API key:", secrets['api'], "Up  Dwn     Ext")
         elif (subscreen == 8):
@@ -255,7 +93,7 @@ def BuildScreen():
 def ShowSystemCheck(screen):
     global subscreen, sc
     if (screen == "start"):
-        Log("Showing Systemcheck")
+        Logger.LogMessage("Showing Systemcheck")
         sc = 0
     elif (screen == "next"):
         sc = sc + 1
@@ -271,91 +109,137 @@ def TogglePartyMode():
     global subscreen, partyMode
     if (partyMode):
         partyMode = False
-        Log("Party-Mode off")
+        Logger.LogMessage("Party-Mode off")
     else:
         partyMode = True
-        Log("Party-Mode on")
+        Logger.LogMessage("Party-Mode on")
     subscreen = 3
+
+def SetPartyMode(newValue):
+    global partyMode
+    partyMode = newValue
+    Logger.LogMessage("Setting Party-Mode via API to " + PartyModeState())
+
+def PartyModeState():
+    global partyMode
+    if (partyMode):
+        return "enabled"
+    else:
+        return "disabled"
     
 def TriggerLicht():
     #todo
     global subscreen
     subscreen = 2
-    Log("Triggering Licht")
+    Logger.LogMessage("Triggering Licht")
     
 def TriggerDoor():
     #todo
     global subscreen
     subscreen = 1
-    Log("Triggering Door")
+    Logger.LogMessage("Triggering Door")
 
-# Hauptroutine, die dauerhaft ausgeführt wird
-async def MainLoop():
-    global interrupt_flag, debounce_time, tasteA, subscreen, boottime, lastActionTicks, displayOff
-    Log("Entering MainLoop")
-    boottime = time.time()
-    ShowText("Booting [3/3]", "API Setup: key", secrets['api'])
-    Log("Setting up API on port " + str(configs['api_port']) + " with key " + secrets['api'])
-    asyncio.create_task(asyncio.start_server(ServeApi, "0.0.0.0", configs['api_port']))
-    ShowText("TCS<->FHEM", "Firmware:", version)
-    Log("Booting complete with Firmware " + version)
+#####################################################################
+
+def set_global_exception():
+    def handle_exception(loop, context):
+        import sys
+        sys.print_exception(context["exception"])
+        sys.exit()
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(handle_exception)
+
+async def UiHandling():
+    global interrupt_flag, lastActionTicks, displayOff, subscreen
     while True:
-        time.sleep(0.5)
         BuildScreen()
-        if interrupt_flag is 1:
-            interrupt_flag = 0
+        if Keypad.interrupt_flag is 1:
+            Keypad.interrupt_flag = 0
             lastActionTicks = 0
             if (displayOff):
                 displayOff = False
             else:
-                interrupt_flag = 0
+                Keypad.interrupt_flag = 0
                 if (subscreen == 0):
-                    if (tasteA == taste1):
+                    if (Keypad.tastePressed == Keypad.taste1):
                         ShowSystemCheck("start")
-                    elif (tasteA == taste2):
+                    elif (Keypad.tastePressed == Keypad.taste2):
                         TogglePartyMode()
-                    elif (tasteA == taste3):
+                    elif (Keypad.tastePressed == Keypad.taste3):
                         TriggerLicht()
-                    elif (tasteA == taste4):
+                    elif (Keypad.tastePressed == Keypad.taste4):
                         TriggerDoor()
                     else:
-                        Log("Error: Invalid Button pressed!")
+                        Logger.LogMessage("Error: Invalid Button pressed!")
                 elif (subscreen == 1 or subscreen == 2 or subscreen == 3):
-                    if (tasteA == taste1):
+                    if (Keypad.tastePressed == Keypad.taste1):
                         subscreen = 0
                 elif (subscreen == 4 or subscreen == 5 or subscreen == 6 or subscreen == 7 or subscreen == 8 or subscreen == 9 or subscreen == 10 or subscreen == 11):
-                    if (tasteA == taste1):
+                    if (Keypad.tastePressed == Keypad.taste1):
                         subscreen = 0
-                    elif (subscreen == 11 and tasteA == taste2):
+                    elif (subscreen == 11 and Keypad.tastePressed == Keypad.taste2):
                         Reboot()
-                    elif (tasteA == taste3):
+                    elif (Keypad.tastePressed == Keypad.taste3):
                         ShowSystemCheck("next")
-                    elif (tasteA == taste4):
+                    elif (Keypad.tastePressed == Keypad.taste4):
                         ShowSystemCheck("prev")
                     else:
-                        Log("Error: Invalid Button pressed!")
+                        Logger.LogMessage("Error: Invalid Button pressed!")
                 else:
-                    if (tasteA == taste1):
+                    if (Keypad.tastePressed == Keypad.taste1):
                         subscreen = 0
+        await asyncio.sleep(0.5)
 
-#####################################################################
+async def APIHandling():
+    #todo
+    while True:
+        v = 3
+        print("api")
+        await asyncio.sleep(1)
+
+async def TCSBusReader():
+    #todo
+    while True:
+        m = 5
+        print("tcs")
+        await asyncio.sleep(5)
+
+async def Main():
+    set_global_exception()
+    Logger.LogMessage("Entering MainLoop")
+    boottime = time.time()
+    loop = asyncio.get_event_loop()
+    ShowText("Booting [3/3]", "API Setup: key", secrets['api'])
+    Logger.LogMessage("Setting up API on port " + str(configs['api_port']) + " with key " + secrets['api'])
+    loop.create_task(APIHandling())
+    #todo
+    ShowText("TCS<->FHEM", "Firmware:", version)
+    Logger.LogMessage("Booting complete with Firmware " + version)
+    loop.create_task(UiHandling())
+    loop.create_task(TCSBusReader())
+    loop.run_forever()
 
 # Booten des Device
 def Boot():
-    HousekeepingLogs()
+    Logger.Housekeeping()
     ShowText("Booting [1/3]", "Conn. Wifi:", secrets['ssid'] + "...")
-    ShowText("Booting [1/3]", "Conn. Wifi: MAC", mac)
-    wlanConnect()
-    if (wlanStatus()):
-        ShowText("Booting [1/3]", "Conn. Wifi: IP", wlan.ifconfig()[0])
+    ShowText("Booting [1/3]", "Conn. Wifi: MAC", Networking.GetMACAddress())
+    Networking.Connect(configs['disable_wifi_powersavingmode'])
+    if (Networking.Status()):
+        ShowText("Booting [1/3]", "Conn. Wifi: IP", Networking.GetIPAddress())
         ShowText("Booting [2/3]", "NTP time:", configs['ntp_host'])
-        setRTCTimeNTP()
-        ShowText("Booting [2/3]", "NTP time:", DateTimeNow())
-        try:
-            asyncio.run(MainLoop())
-        finally:
-            asyncio.new_event_loop()
+        NTP.SetRTCTimeFromNTP(configs['ntp_host'], configs['gmt_offset'], configs['auto_summertime'])
+        ShowText("Booting [2/3]", "NTP time:", TimeUtils.DateTimeNow())
     else:
         ShowText("Booting [1/3]", "Conn. Wifi:", "ERROR!")
+
+#####################################################################
         
 Boot()
+try:
+    asyncio.run(Main())
+finally:
+    asyncio.new_event_loop()
+
+#test things down here
+Logger.LogMessage("Ende erreicht")
